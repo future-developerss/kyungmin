7949664534246864debc4b237f06a77185872e91
min2000 <min2000.lee@gmail.com>
2021-03-09 13:28:15 +0900

    백준 1001번: 입출력(-)

---
+++ minus1001.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class minus1001 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);  //System.in: 입력 값
+		                                    //Scanner는 입력 받을 때 쓰임 (정수, 실수, 문자열 읽어 올 수 있다)
+		
+		int x=in.nextInt();  //int를 입력 받을 때 nextInt 메서드를 사용
+		int y=in.nextInt();
+		
+		in.close();
+		
+		int res=x-y;
+		System.out.println(res);
+
+	}
+
+}
