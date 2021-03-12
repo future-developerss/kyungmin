8dfb40d20a7ec8fc319c5edd2883863136378ef3
min2000 <min2000.lee@gmail.com>
2021-03-12 17:49:09 +0900

    백준 2742번: for문 (기찍N)

---
+++ ForN2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ForN2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		int num=sc.nextInt();
+		
+		for (int i=num; i>=1; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
