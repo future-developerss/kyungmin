23e60f43d69e8c18ac8772fe184479883235d0de
min2000 <min2000.lee@gmail.com>
2021-03-22 19:20:12 +0900

    백준 11654: 문자열(아스키코드)

---
+++ ASCII11654.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ASCII11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		char ch=sc.nextLine().charAt(0);  //index=0인 문자
+		int num=(int)ch;
+		
+		System.out.println(num);
+	}
+
+}
